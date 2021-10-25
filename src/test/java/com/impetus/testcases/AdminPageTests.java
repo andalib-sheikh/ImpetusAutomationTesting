82200c680e62f5ecd48efeeb1cf16b2e7a60268c
unknown <andalib.sheikh22062@gmail.com>
2021-10-25 13:25:32 +0530

    Initial Project

--- src/test/java/com/impetus/testcases/AdminPageTests.java
+++ src/test/java/com/impetus/testcases/AdminPageTests.java
@@ -43,10 +43,13 @@ public class AdminPageTests extends BasePage{
 	}
 	
 	@Test(priority=1, dataProvider="getTest01Data", enabled=true)
-	public void test01(String Min_Exp, String Max_Exp, String Country, String Keyword, String JobTitle) throws Exception
+	public void test01(String Min_Exp, String Max_Exp, String Country, String Keyword, String JobTitle,
+			String FirstName, String LastName, String EmailAddress, String PhoneNo, String CurrentLocation, String PreferredLocation,
+			String CurrentCTC, String ExpectedCTC, String PrimarySkills, String SecondarySkills, String NoticePeriod) throws Exception
 	{
 		try {
-		adminPage.test01(Min_Exp, Max_Exp, Country, Keyword, JobTitle);
+		adminPage.test01(Min_Exp, Max_Exp, Country, Keyword, JobTitle, FirstName, LastName, EmailAddress, PhoneNo, CurrentLocation, PreferredLocation,
+			 CurrentCTC, ExpectedCTC, PrimarySkills, SecondarySkills, NoticePeriod);
 		Assert.assertEquals(adminPage.text, "Success");
 		}
 		catch(Exception e)
