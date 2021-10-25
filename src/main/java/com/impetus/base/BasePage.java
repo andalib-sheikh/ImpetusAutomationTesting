6d95d4111b5dee7559bc2e019cafbf9dcb7f92be
andalib-sheikh <andalib.sheikh22062@gmail.com>
2021-10-25 11:11:15 +0530

    Update BasePage.java

--- src/main/java/com/impetus/base/BasePage.java
+++ src/main/java/com/impetus/base/BasePage.java
@@ -1,11 +1,16 @@
 package com.impetus.base;
 
 import java.io.FileInputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import com.impetus.utils.Timeout;
 
@@ -31,7 +36,7 @@ public class BasePage {
 			e.printStackTrace();
 		}
 	}	
-	public static void initialization()
+	/*public static void initialization()
 	{
 		System.setProperty("webdriver.edge.driver",prop.getProperty("edgeDriverPath"));
 		driver=new EdgeDriver();
@@ -40,7 +45,28 @@ public class BasePage {
 		driver.manage().timeouts().pageLoadTimeout(Timeout.PAGE_LOAD_TIMEOUT, TimeUnit.SECONDS);
 		driver.manage().timeouts().implicitlyWait(Timeout.IMPLICIT_WAIT, TimeUnit.SECONDS);
 		driver.get(prop.getProperty("url"));
-	}
+	}*/
 	
+	public static void initialization() throws MalformedURLException
+	{
+		MutableCapabilities sauceOptions=new MutableCapabilities();
+		sauceOptions.setCapability("name", "Impetus Automation Tests");
+		DesiredCapabilities cap= new DesiredCapabilities();
+		cap.setCapability("sauce:options", sauceOptions);
+		cap.setCapability("platformName", "Windows 10");
+		cap.setCapability("browserName", "microsoftedge");
+		cap.setCapability("browserVersion", "92");
+		
+		
+		System.setProperty("webdriver.edge.driver","build/msedgedriver");
+		String huburl="https://oauth-andalib.sheikh22062-25b67:b486ce09-bf02-4979-a32e-4d81788dc344@ondemand.eu-central-1.saucelabs.com:443/wd/hub";
+		driver=new RemoteWebDriver(new URL(huburl),cap);
+
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(Timeout.PAGE_LOAD_TIMEOUT, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(Timeout.IMPLICIT_WAIT, TimeUnit.SECONDS);
+		driver.navigate().to(prop.getProperty("url"));
+	}
 	
 }
