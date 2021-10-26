def2ad6ff2fb4b5838568a9b773cf61ee122bdb9
andalib-sheikh <andalib.sheikh22062@gmail.com>
2021-10-26 20:46:55 +0530

    Update AdminPage.java

--- src/main/java/com/impetus/pages/AdminPage.java
+++ src/main/java/com/impetus/pages/AdminPage.java
@@ -127,7 +127,7 @@ public class AdminPage extends BasePage{
 		Thread.sleep(3000);
 		elementUtil.waitForElementToBeVisible(lblUploadResume);
 		Thread.sleep(2000);
-		String filePath ="/src/main/java/TestData/Andalib%20Sheikh_Resume_October2021.pdf";
+		String filePath ="src/main/java/TestData/Andalib Sheikh_Resume_October2021.pdf";
 		btnUploadResume.sendKeys(filePath);
 		Thread.sleep(3000);
 		inputFirstName.clear();
