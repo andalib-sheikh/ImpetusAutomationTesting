bc3541e919d41093fe27d6db6d90b721d20233c7
andalib-sheikh <andalib.sheikh22062@gmail.com>
2021-10-27 10:31:57 +0530

    Update AdminPage.java

--- src/main/java/com/impetus/pages/AdminPage.java
+++ src/main/java/com/impetus/pages/AdminPage.java
@@ -134,7 +134,7 @@ public class AdminPage extends BasePage{
 		//((RemoteWebElement)btnUploadResume).setFileDetector(new LocalFileDetector());
 		((RemoteWebDriver)driver).setFileDetector(new LocalFileDetector());
 		System.out.println("SSSSSSSSSSSSSSSSSSSSS "+System.getProperty("user.dir"));
-		File file = new File("./src/main/java/TestData/TestData.xlsx"); 
+		File file = new File("./src/main/java/TestData/AS_Resume.pdf"); 
 	    System.out.println("file exists: " + file.exists());
 
 	    String imagePath = file.getAbsolutePath();
